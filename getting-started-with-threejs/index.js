6456beb09f5b8ece31d25f5d1d0a11b62dec0bc4
Prakhar Sharma <prakhar.sharma@ukaea.uk>
2026-06-10 10:54:29 +0100

    first commit

---
+++ getting-started-with-threejs/index.js
@@ -0,0 +1,61 @@
+console.log('A basic primitive');
+import * as THREE from "three";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js'; // to add mouse movements
+
+const w = window.innerWidth;
+const h = window.innerHeight; 
+const renderer = new THREE.WebGLRenderer({antialias: true});
+
+renderer.setSize(w, h);
+
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w/h;
+const near = 0.1;
+const far = 10;
+
+
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+// Basic background added
+const controls = new OrbitControls(camera, renderer.domElement);
+// add smooth deceleration to the animation when mouse is used to move the object.
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+const geo = new THREE.IcosahedronGeometry(1,8);
+const mat = new THREE.MeshStandardMaterial({
+    color: "white",
+    flatShading: true,
+});
+const mesh = new THREE.Mesh(geo, mat);
+
+scene.add(mesh);
+
+const wireMat = new THREE.MeshBasicMaterial({
+    color: "white",
+    wireframe: true
+})
+const wireMesh = new THREE.Mesh(geo, wireMat);
+mesh.add(wireMesh); // combine the wirefame mesh with geo mesh
+
+const hemiLight = new THREE.HemisphereLight("blue", "cyan");
+scene.add(hemiLight);
+
+function animate(t = 0){
+    requestAnimationFrame(animate);
+    // console.log(t);
+    // mesh.scale.setScalar(Math.cos(t*0.01));
+    mesh.rotation.y = t*0.0005;
+    // mesh.rotation.x = t*0.0001;
+
+    controls.update();
+    renderer.render(scene, camera);
+
+}
+animate();
+// renderer.render(scene, camera)
